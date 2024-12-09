d34c61cfffdfad4b96a0f85ea545f8c1bbedba60
mahmoudossama10 <59567436+mahmoudossama10@users.noreply.github.com>
2024-12-09 14:08:09 +0200

    Optimized code for 20m

--- vec_db.py
+++ vec_db.py
@@ -2,19 +2,25 @@ from typing import Dict, List, Annotated
 import numpy as np
 import os
 import struct
-import gc
 from IvfTrain import IvfTrain
+from sortedcontainers import SortedList
+#from itertools import chain
 
 DB_SEED_NUMBER = 42
 ELEMENT_SIZE = np.dtype(np.float32).itemsize
 DIMENSION = 70
 
 class VecDB:
-    def __init__(self, database_file_path = "saved_db.dat", index_file_path =  ["saved_clusters.dat", "saved_centroids.dat", "saved_indexes.dat"], new_db = True, db_size = None) -> None:
+    def __init__(self, database_file_path = "saved_db.dat", index_file_path = "index_path", new_db = True, db_size = None) -> None:
         self.db_path = database_file_path
-        self.cluster_path = index_file_path[0]
-        self.centroid_path = index_file_path[1]
-        self.index_path = index_file_path[2]
+        self.general_path = index_file_path
+        self.size = 2
+        self.db_size = db_size
+        # print(index_file_path[0])
+        # print(index_file_path[1])
+        # print(index_file_path[2])
+        #print(self.index_path)
+        #self._build_index()
         if new_db:
             if db_size is None:
                 raise ValueError("You need to provide the size of the database")
@@ -22,7 +28,6 @@ class VecDB:
             if os.path.exists(self.db_path):
                 os.remove(self.db_path)
             self.generate_database(db_size)
-        
     
     def generate_database(self, size: int) -> None:
         rng = np.random.default_rng(DB_SEED_NUMBER)
@@ -70,15 +75,12 @@ class VecDB:
         vectors = np.memmap(self.db_path, dtype=np.float32, mode='r', shape=(num_records, DIMENSION))
         return np.array(vectors[:,1:])
 
-        def get_multiple_rows(self, ranged_clusters_ids):
+
+    def get_multiple_rows(self, ranged_clusters_ids):
         ranged_clusters = []
-        # print(ranged_clusters_ids)
         with open(self.db_path, 'rb') as file:
             for id in ranged_clusters_ids:
-                #print(id)
                 offset = np.int64(id[1]) * DIMENSION * ELEMENT_SIZE
-                # print(id[1])
-                # print(offset)
                 file.seek(offset)
                 packed_data = file.read(DIMENSION * ELEMENT_SIZE)
                 unpacked_data = struct.unpack(f'{DIMENSION}f', packed_data)
@@ -88,55 +90,74 @@ class VecDB:
             del file
         return ranged_clusters
     
+    def divide_into_batches(self, arr, batch_size):
+        length = len(arr)
+        return [arr[i:i + batch_size] for i in range(0, length, batch_size)]
+    
+    def ceil(self, x):
+        integer_part = x - (x % 1)
+        return integer_part + (1 if x % 1 > 0 else 0)
+        
     def retrieve(self, query: Annotated[np.ndarray, (1, DIMENSION)], top_k = 5):
         scores = []
         
         centroids = []
-        file = open("saved_centroids.dat", 'rb')
+        file = open(f"{self.general_path}/saved_centroids.dat", 'rb')
         try:
             row_size = ELEMENT_SIZE * (DIMENSION + 1)
             data = file.read()
             length = len(data)
             for offset in range(0, length, row_size):
-                packed_data = data[offset:offset + row_size]
-                if len(packed_data) < row_size:
+                if len(data[offset:offset + row_size]) < row_size:
                     break
-                unpacked_data = struct.unpack(f'i{DIMENSION}f', packed_data)
-                del packed_data
-                
+                unpacked_data = struct.unpack(f'i{DIMENSION}f', data[offset:offset + row_size])
                 centroids.append(unpacked_data)
-            
         finally:
             file.close()
             del file
-            
         centroids = np.array(centroids)
-        sorted_indices = np.argsort((np.dot(centroids[:, 1:], query.T).T / (np.linalg.norm(centroids[:, 1:], axis=1) * np.linalg.norm(query))).squeeze())[::-1]
-
-        best_centroids = sorted_indices.tolist()
-
-        del sorted_indices
+        query = np.array(query)
+        top_60_centroids = SortedList(key=lambda x: -x[0]) 
+        for i, centroid in enumerate(centroids):
+            score = np.dot(centroid[1:], query.T) / (np.linalg.norm(centroid[1:]) * np.linalg.norm(query))
+            if len(top_60_centroids) < 60:
+                top_60_centroids.add((score, i))
+            else:
+                if score > -top_60_centroids[-1][0]:
+                    top_60_centroids.add((score, i))
+                    top_60_centroids.pop() 
+        best_centroids = [item[1] for item in top_60_centroids]
         
-        scores = best_centroids[:4]
+        del top_60_centroids
+        #del sorted_indices
+        
+        if self._get_num_records() == 20000000:
+            #print(top_k)
+            scores = best_centroids[:5]
+        elif self._get_num_records() == 15000000:
+            scores = best_centroids[:50]
+        elif self._get_num_records() == 10000000:
+            scores = best_centroids[:60]
+        else:
+            scores = best_centroids[:60]
         del best_centroids
         top_k_results = []
         for score in scores:
             first_index, second_index = None, None
-            file = open("saved_indexes.dat", 'rb')
+            file = open(f"{self.general_path}/saved_indexes.dat", 'rb')
             try:
                 position = 3 * score * ELEMENT_SIZE
                 file.seek(int(position))
                 packed_data = file.read(3 * ELEMENT_SIZE)
+
                 unpacked_data = struct.unpack('iii', packed_data)
                 del packed_data
                 first_index, second_index = unpacked_data[1], unpacked_data[2]
-                
             finally:
                 file.close()
                 del file
-                
             ranged_clusters_ids = []
-            with open("saved_clusters.dat", 'rb') as file:
+            with open(f"{self.general_path}/saved_clusters.dat", 'rb') as file:
                 file.seek(first_index)
                 while file.tell() < second_index:
                     packed_data = file.read(2 * ELEMENT_SIZE)
@@ -145,23 +166,18 @@ class VecDB:
                     data = struct.unpack('ii', packed_data)
                     del packed_data
                     ranged_clusters_ids.append(data)
-                    
                 file.close()
                 del file
-                
-            
             ranged_clusters = self.get_multiple_rows(ranged_clusters_ids)
-            cosine_similarities = []
+            best_vectors = SortedList(key=lambda x: -x[0])
             for row in ranged_clusters:
                 cosine_similarity = self._cal_score(query, row[0])
-                cosine_similarities.append((cosine_similarity, row[1]))
-            
-            top_k_results.extend(cosine_similarities)
-            del cosine_similarities
-            
-        print(len(top_k_results))
+                best_vectors.add((cosine_similarity, row[1]))
+            length = len(ranged_clusters_ids)
+            top_k_results.extend(best_vectors[:min(top_k, length)])
+            del length
+            del best_vectors
         scores = sorted(top_k_results, key=lambda x: x[0], reverse=True)[:top_k]
-        gc.collect()
         return [s[1] for s in scores]
     
     def _cal_score(self, vec1, vec2):
@@ -169,7 +185,11 @@ class VecDB:
         norm_vec1 = np.linalg.norm(vec1)
         norm_vec2 = np.linalg.norm(vec2)
         cosine_similarity = dot_product / (norm_vec1 * norm_vec2)
+        del dot_product
+        del norm_vec1
+        del norm_vec2
         return cosine_similarity
 
     def _build_index(self):
-        IvfTrain()
+        # Placeholder for index building logic
+        lol2 = IvfTrain()
