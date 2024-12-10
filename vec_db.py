e5a74888562b40c1754608dbaec325c307f6d1ec
mahmoudossama10 <59567436+mahmoudossama10@users.noreply.github.com>
2024-12-10 13:18:00 +0200

    bug fixes for centroids

--- vec_db.py
+++ vec_db.py
@@ -135,11 +135,11 @@ class VecDB:
         
         if self._get_num_records() == 20000000:
             #print(top_k)
-            scores = best_centroids[:5]
+            scores = best_centroids[:4]
         elif self._get_num_records() == 15000000:
             scores = best_centroids[:50]
         elif self._get_num_records() == 10000000:
-            scores = best_centroids[:60]
+            scores = best_centroids[:50]
         else:
             scores = best_centroids[:60]
         del best_centroids
